6b4ef0037fa0dc5287afddc8752e9f7271d0cf7c
Wuyetz <40246730+Wuyetz@users.noreply.github.com>
2018-06-21 22:50:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+﻿window.onload = function () {
+
+
+function Player (name){
+    
+this.name = name;
+
+}
+
+
+var playerArray = [];
+
+
+//var value=$('#newPlayer').val();
+
+
+//var obj = JSON.parse(value);
+
+
+$(function () {
+        
+$('form[name="newPlayer"]').submit(function() {
+            var player1 = $(this.name).serializeArray();
+            // Do what you will with 'person' now...
+        });
+    });
+    
+players.onclick = function(){
+    alert(playerArray.length);    
+};
+    
+};
