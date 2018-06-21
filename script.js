52f6eac5b017adabe4d8b57da0b6ad3a0973df41
Wuyetz <40246730+Wuyetz@users.noreply.github.com>
2018-06-21 23:44:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-﻿window.onload = function () {
-
+window.onload = function () {
 
 function Player (name){
-    
-this.name = name;
-
+    this.name = name;
 }
-
 
 var playerArray = [];
 
-
 //var value=$('#newPlayer').val();
 
-
 //var obj = JSON.parse(value);
-
-
+/*
 $(function () {
-        
-$('form[name="newPlayer"]').submit(function() {
-            var player1 = $(this).serializeArray();
-            // Do what you will with 'person' now...
+        $('form[name="newPlayer"]').submit(function() {
+            var player1 = $(this.name).serializeArray();
             playerArray.push(player1);
+            // Do what you will with 'person' now...
         });
     });
+*/
+/*
+var player1 = {};
+formData.forEach(function(value, key){
+    object[key] = value;
+});
+var json = JSON.stringify(player1);
+*/
+/*
+sbmt.onclick = function(){
+document.getElementById("newPlayer").value = new Player(this.value);
+};
+*/
+sbmt.onsubmit = function(){
+    playerArray.push(window[$('form[name="newPlayer"]').value] = new Player());
+    
+};
+//window[variableName] = new Player();
 
-
-     
 players.onclick = function(){
+  //  playerArray.push(player1);
     alert(playerArray.length);    
 };
     
