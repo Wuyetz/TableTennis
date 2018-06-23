2cffecafa9038849fe8ef632091c835a72492ed4
Wuyetz <40246730+Wuyetz@users.noreply.github.com>
2018-06-23 22:35:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 window.onload = function(){
-    
-//    $("#matchResults").toggle();
-
-
 
 
 const playerName = document.getElementById("playerName");
@@ -10,14 +6,14 @@ const submitPlayer = document.getElementById("submitPlayer");
 const selectA = document.getElementById('selectA');
 const selectB = document.getElementById('selectB');
 const submitMatch = document.getElementById("submitMatch");
-const matchResult = document.getElementById("matchResult");
+//const matchResult = document.getElementById("matchResult");
 const a1 = document.getElementById("a1");
 
 
 
 const players = [];
 
-
+document.getElementById("submitMatch").disabled = true;
 
 
 class Player {
@@ -36,36 +32,15 @@ submitPlayer.addEventListener('click', function() {
       document.getElementById("morePlayers").innerHTML = "";
   }
 });
-/*
-match.onclick = function(){
-    $("#matchResults").toggle();
-};
-*/
 
-document.getElementById("submitMatch").disabled = true;
 
-function setResult (a,b,p) {
-    
-    
-    if(((a-b)>1||(b-a)>1)&&(a||b>10)){
-/*        if(a>b){
-            $( "#selectA" ).val().wins+1;
-            $( "#selectB" ).val().losses+1;
-        }else{
-            $( "#selectB" ).val().wins+1;
-            $( "#selectA" ).val().losses+1;
-        }*/
-        
-    p = "woooo";    
-    }else{
-        p="Please enter correct scores for this set!";
-    }
-}
 
-$(document).on('change' , '.sets', function(){
-   /* var x = $("a1").val();
-       var y = $("b1").val();
-    if(x&&y){alert("decf");}*/
+
+
+
+
+$(document).on('change' , '.sets' , '.playerSelect', function(){
+
     var a1 = Number(matchResult.a1.value);
     var a2 = Number(matchResult.a2.value);
     var a3 = Number(matchResult.a3.value);
@@ -85,14 +60,21 @@ $(document).on('change' , '.sets', function(){
     var p5 = document.getElementById("p5");
     
     var ws = " wins this set!";
+    
+    var aPoints = 0;
+    var bPoints = 0;
+    
+    
 
 if(selectA.value!==""&&selectB.value!=="")
     {
     if((((a1-b1)>1||(b1-a1)>1)&&((a1==11||b1==11)))||(((a1-b1)==2||(b1-a1)==2)&&(a1>11||b1>11))){
         if(a1>b1){
             p1.innerHTML = selectA.value+ws;
+            aPoints+=1;
         }else{
             p1.innerHTML = selectB.value+ws;
+            bPoints+=1;
         }
     }else{
         p1.innerHTML ="";
@@ -101,8 +83,10 @@ if(selectA.value!==""&&selectB.value!=="")
     if((((a2-b2)>1||(b2-a2)>1)&&((a2==11||b2==11)))||(((a2-b2)==2||(b2-a2)==2)&&(a2>11||b2>11))){
         if(a2>b2){
             p2.innerHTML = selectA.value+ws;
+            aPoints+=1;
         }else{
             p2.innerHTML = selectB.value+ws;
+            bPoints+=1;
         }
     }else{
         p2.innerHTML ="";    
@@ -111,8 +95,10 @@ if(selectA.value!==""&&selectB.value!=="")
     if((((a3-b3)>1||(b3-a3)>1)&&((a3==11||b3==11)))||(((a3-b3)==2||(b3-a3)==2)&&(a3>11||b3>11))){
         if(a3>b3){
             p3.innerHTML = selectA.value+ws;
+            aPoints+=1;
         }else{
             p3.innerHTML = selectB.value+ws;
+            bPoints+=1;
         }
     }else{
         p3.innerHTML ="";    
@@ -121,8 +107,10 @@ if(selectA.value!==""&&selectB.value!=="")
     if((((a4-b4)>1||(b4-a4)>1)&&((a4==11||b4==11)))||(((a4-b4)==2||(b4-a4)==2)&&(a4>11||b4>11))){
         if(a4>b4){
             p4.innerHTML = selectA.value+ws;
+            aPoints+=1;
         }else{
             p4.innerHTML = selectB.value+ws;
+            bPoints+=1;
         }
     }else{
         p4.innerHTML ="";    
@@ -131,138 +119,72 @@ if(selectA.value!==""&&selectB.value!=="")
     if((((a5-b5)>1||(b5-a5)>1)&&((a5==11||b5==11)))||(((a5-b5)==2||(b5-a5)==2)&&(a5>11||b5>11))){
         if(a5>b5){
             p5.innerHTML = selectA.value+ws;
+            aPoints+=1;
         }else{
             p5.innerHTML = selectB.value+ws;
+            bPoints+=1;
         }
     }else{
         p5.innerHTML ="";    
     }
-    }
     
-//    setResult(x,y,p1);      
-    
-    
- //   alert(typeof(x));
-//    alert(x-y);
-
-    
-});
-/*
-$(".one").on("change", function (e) {
-    
-    var x = $("a1").val();
-       var y = $("b1").val();
- //      var z = $("p1").innerHTML;
- if(x&&y){alert(x+y);}
-    
-});*/
-//alert("vdsvsd");
-       
- //      if(document.getElementById("a1")&&document.getElementById("a1").value&&document.getElementById("b1")&&document//.getElementById("b1").value){
-// alert("vdefv");
- 
- /*      
-       var x = document.getElementById("a1").value;
-       var y = document.getElementById("b1").value;
-       var z = document.getElementById("p1").innerHTML;
-       
-       
-       setResult(x,y,z);*/
-//}
-    
-//});
+if((aPoints>2||bPoints>2)&&(((aPoints-bPoints)<4)||((bPoints-aPoints)<4))&&(selectA.value!==selectB.value)){
+    alert("wooooooo");
+}
+}});
 
 
 /*
+
 ('#matchResult').submit(function(e) {
     e.preventDefault();
-    var plA = $( "#selectA" ).val();
-    var plB = $( "#selectB" ).val();
-    var aPoints = 0;
-    var bPoints = 0;
-    $.validator.addMethod(
-    "set",
-    function (value, element, params) {
-        var sumOfVals = 0;
-        var parent = $(element).parent(".parentDiv");
-        $(parent).find("input").each(function () {
-            sumOfVals = sumOfVals + parseInt($(this).val(), 10);
-        });
-        if (sumOfVals == params) return true;
-        return false;
-    },
-    jQuery.format("Sum must be {0}")
-);
-}).validate({
+    }).validate({
     rules: {
-    a1: {
-      required: true,
-      range: [0, Infinity]
-    },
-    a2: {
-      required: true,
-      range: [0, Infinity]
-    },
-    a3: {
-      required: true,
-      range: [0, Infinity]
-    },
-    a4: {
-      required: true,
-      range: [0, Infinity]
-    },
-    a5: {
-      required: true,
-      range: [0, Infinity]
-    },
-    b1: {
-      required: true,
-      range: [0, Infinity]
-    },
-    b2: {
-      required: true,
-      range: [0, Infinity]
-    },
-    b3: {
-      required: true,
-      range: [0, Infinity]
-    },
-    b4: {
-      required: true,
-      range: [0, Infinity]
-    },
-    b5: {
-      required: true,
-      range: [0, Infinity]
-    },
-    selectA: "required",
-    selectB: "required",
     
+          selectA:
+          {
+            required: true
+          },
+          selectB:
+          {
+            required: true  
+          },
+          p1:
+          {
+            minlength: 10
+          },
+          p2:
+          {
+            minlength: 10  
+          },
+          p3:
+          {
+            minlength: 10  
+          },
+          p4:
+          {
+            minlength: 10  
+          },
+          p5:
+          {
+            minlength: 10  
+          }
+        },
+    messages: {
+    selectA: 'This field is required',
+    selectB: 'This field is required',
+    p1: { minlength: 'Enter a valid score for set 1'},
+    p2: { minlength: 'Enter a valid score for set 2'},
+    p3: { minlength: 'Enter a valid score for set 3'},
+    p4: { minlength: 'Enter a valid score for set 4'},
+    p5: { minlength: 'Enter a valid score for set 5'}
     
-    
-/*
-    if($( "a1" ).val()>$( "#b1" ).val()){aPoints+1}
-    if($( "a2" ).val()>$( "#b2" ).val()){aPoints+1}
-    if($( "a3" ).val()>$( "#b3" ).val()){aPoints+1}
-    if($( "a4" ).val()>$( "#b4" ).val()){aPoints+1}
-    if($( "a5" ).val()>$( "#b5" ).val()){aPoints+1}
-    if($( "b1" ).val()>$( "#a1" ).val()){bPoints+1}
-    if($( "b2" ).val()>$( "#a2" ).val()){bPoints+1}
-    if($( "b3" ).val()>$( "#a3" ).val()){bPoints+1}
-    if($( "b4" ).val()>$( "#a4" ).val()){bPoints+1}
-    if($( "b5" ).val()>$( "#a5" ).val()){bPoints+1}
-    if(aPoints>bPoints){
-        plA.wins+1;
-    }
-    if(bPoints>aPoints)*/
-//    $(".error").remove();
-//    document.getElementById("matchResults").reset();
-//}});
+}});
+*/    
 
 
 
 
-//$(document).ready(function() { 
 $("#selectA").on("focus", function (e) {
     $('#selectA')
     .empty();
@@ -278,11 +200,6 @@ selectA.appendChild(option);
 });
 
 
-$("#selectA").on("blur", function (e) {
-    
-
-    if(selectA.value===selectB.value){alert("womp womp")}
-});
 
 
 $("#selectB").on("focus", function (e) {
